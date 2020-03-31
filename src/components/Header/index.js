9917f1f743c05fb96b9fcfb9c39d9d4554fadd68
Miguel Aspis <aspismiguel2@gmail.com>
2020-03-31 17:09:01 -0300

    pipefy-clone

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+
+export default function Header() {
+    return (
+        <div>
+            <Container>
+                <h1>Pipefy-Clone</h1>
+            </Container>
+        </div>
+    )
+}
